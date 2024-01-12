ab0996ec3f4f43df357f65eb06fa5825fe0484d4
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-01-12 18:55:26 -0300

    init api

---
+++ src/test/java/dev/igor/apiaccount/ApiAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igor.apiaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
