941a5d6003eee8bea0538411d0ea8d3520d58e40
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-01-14 17:24:28 -0300

    receive transaction from queue and decrease balance

--- src/main/java/dev/igor/apiaccount/event/OutcomeTransaction.java
+++ src/main/java/dev/igor/apiaccount/event/OutcomeTransaction.java
@@ -4,13 +4,30 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.log4j.Log4j2;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.igor.apiaccount.dto.Transaction;
+import dev.igor.apiaccount.service.OutcomeService;
 
 @Component
-@Log4j2
 public class OutcomeTransaction {
+    private final OutcomeService service;
+    private final ObjectMapper mapper;
+
+    public OutcomeTransaction(ObjectMapper mapper, OutcomeService service) {
+        this.mapper = mapper;
+        this.service = service;
+    }
+
     @RabbitListener(queues = {"file-teste"})
     public void receive(@Payload String message) {
-        log.info(message);
+        Transaction transaction;
+        try {
+            transaction = mapper.readValue(message, Transaction.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to generate Object Java with message");
+        }
+        service.outcome(transaction);
     }
 }   
---
+++ src/main/java/dev/igor/apiaccount/service/OutcomeService.java
@@ -0,0 +1,7 @@
+package dev.igor.apiaccount.service;
+
+import dev.igor.apiaccount.dto.Transaction;
+
+public interface OutcomeService {
+    void outcome(Transaction transaction);
+}
